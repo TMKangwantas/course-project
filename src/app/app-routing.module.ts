fb232d7f4c4a8a8ed0ad625029bd30c7d03dceda
Mark Kangwantas <TMKangwantas@gmail.com>
2019-12-16 16:05:32 -0500

    Day 7: Utilized HTTP requests and also added resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipesResolverService } from './recipes/recipe-start/recipes-resolver.service';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
@@ -12,8 +13,8 @@ const appRoutes: Routes = [
         children: [
             {path: '', component: RecipeStartComponent},
             {path: 'new', component: RecipeEditComponent},
-            {path: ':id', component: RecipeDetailComponent},
-            {path: ':id/edit', component: RecipeEditComponent}
+            {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
+            {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
         ]
     },
     {path: 'shoppingList', component: ShoppingListComponent}
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -5,24 +5,26 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel', 
-      'This is simply a test', 
-      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-    new Recipe(
-      'Big Fat Burger', 
-      'This is simply another test', 
-      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ])
-    ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel', 
+  //     'This is simply a test', 
+  //     'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
+  //   new Recipe(
+  //     'Big Fat Burger', 
+  //     'This is simply another test', 
+  //     'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Meat', 1)
+  //     ])
+  //   ];
+  
+  private recipes: Recipe[] = [];
 
   getRecipes() {
       return this.recipes.slice(); 
@@ -30,6 +32,11 @@ export class RecipeService {
 
   getRecipe(index: number) {
     return this.recipes[index];
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   addRecipe(recipe: Recipe) {
